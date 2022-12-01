c322cd02271ca3f80af7646f60ebdc5331df63ed
marvelyko <karlo.tevzadze.1@iliauni.edu.ge>
2022-12-01 20:02:40 +0400

    add main.html, add images and content

--- omnigether/urls.py
+++ omnigether/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from main.views import index
+from django.conf.urls.static import static
+from django.conf import settings
 
+from main.views import main_view,home_view
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index),
+    path('',home_view,name='home'),
+    path('main/',main_view,name='main')
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
